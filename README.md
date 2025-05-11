fc5c24b36dc382b27b3dde7dcfc122cf769ab7f5
CrazySlots <83612508+CrazySlots@users.noreply.github.com>
2025-05-11 09:42:02 -0600

    Add files via upload
    asdas

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
+# coding-project-template
+=======
 # Lab_React
+>>>>>>> 47036776d5f527f9eeff8b481df64499ec012737
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () =>{
+    return(
+     <>
+     <footer>
+        <div>
+            This is the footer for the site.
+            <p>
+                We will be more than happy to help you plan you next event.
+            </p>
+        </div>
+        
+     </footer>
+     </>   
+    )
+};
+export default Footer;
